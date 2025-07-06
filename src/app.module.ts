3922d3da36cf9182f34095f67c2725da048ddbf5
Aditya Tripathi <atri0048@student.monash.edu>
2025-07-06 22:29:30 +0530

    refactor: remove Redis cache configuration and unused imports from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -12,33 +10,11 @@ import { WebsocketsModule } from './websockets/websockets.module';
 import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
 import { PoemsModule } from './poems/poems.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-
-    CacheModule.register({
-      isGlobal: true,
-      useFactory: async () => {
-        const store = await redisStore({
-          socket: {
-            host: process.env.REDISHOST!,
-            port: process.env.REDISPORT
-              ? parseInt(process.env.REDISPORT)
-              : 6379,
-          },
-          url: process.env.REDIS_URL,
-          password: process.env.REDISPASSWORD,
-        });
-        return {
-          store: store as unknown as CacheStorage,
-          ttl: 60 * 60 * 24 * 7,
-        };
-      },
     }),
     PrismaModule,
     JwtModule.register({}),
@@ -48,7 +24,6 @@ import { redisStore } from 'cache-manager-redis-store';
     UserModule,
     PoemsModule,
   ],
-  controllers: [AppController, AuthController],
   providers: [AppService, AuthService, PrismaService, JwtService],
 })
 export class AppModule {}
