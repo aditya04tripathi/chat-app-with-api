5c262ea59d4fed3bd3eeab1d691fa12afb75e000
Aditya Tripathi <atri0048@student.monash.edu>
2025-07-06 21:31:36 +0530

    Refactor code structure for improved readability and maintainability

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,10 +2,7 @@ function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="w-4/5 h-4/5 border rounded flex flex-col md:flex-row items-center justify-center overflow-hidden">
-        <img
-          src="/auth.jpg"
-          className="hidden md:block w-1/2 h-full object-cover"
-        />
+        <img src="/assets/auth.jpg" className="hidden md:block w-1/2 h-full object-cover" />
 
         <div className="w-full md:w-1/2">{children}</div>
       </div>
